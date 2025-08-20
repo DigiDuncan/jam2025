c9ad2435487e1f5652e9e91733ab56caea88f4ca
DigiDuncan <digiduncan@gmail.com>
2025-08-20 03:01:42 -0400

    prepare input icons

--- jam2025/views/calibration_step1.py
+++ jam2025/views/calibration_step1.py
@@ -1,4 +1,4 @@
-from arcade import XYWH, View, Window
+from arcade import XYWH, View
 
 from jam2025.core.popup import Popup
 
@@ -7,9 +7,11 @@ class MouseCalibrationView(View):
     def __init__(self) -> None:
         super().__init__()
 
-        popup_rect = XYWH(self.center_x, self.center_y, 100, 100)
-        self.popup = Popup(popup_rect)
-        self.popup.popup()
+        popup_rect = XYWH(self.center_x, self.center_y, 75, 75)
+        popup_rect = popup_rect.align_right(self.width - 25)
+        popup_rect = popup_rect.align_bottom(25)
+        self.popup = Popup(popup_rect, fade_in = 1.0, hold = 3.0, fade_out = 1.0)
+        self.popup.popup("mouse")
 
     def on_draw(self) -> bool | None:
         self.clear()

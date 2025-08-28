436c3e54fa284a5b0c1b3fdf1e384184aae76f80
DigiDuncan <digiduncan@gmail.com>
2025-08-28 01:48:43 -0400

    bulletlist

--- jam2025/core/game/bullet.py
+++ jam2025/core/game/bullet.py
@@ -1,6 +1,6 @@
 from typing import Protocol
 
-from arcade import SpriteCircle
+from arcade import SpriteCircle, SpriteList
 from arcade.clock import GLOBAL_CLOCK
 from arcade.types import Point2
 import arcade
@@ -17,12 +17,12 @@ class Character(Protocol):
 class Bullet:
     def __init__(self, radius: float = 10, damage: float = 1, live_time: float = 10) -> None:
         self.sprite = SpriteCircle(radius, arcade.color.RED)  #type: ignore -- float -> int
-        self.velocity = (0, 0)
+        self.velocity: Point2 = (0, 0)
         self.damage = damage
         self.live = True
         self.live_time = live_time
 
-        self.creation_time = GLOBAL_CLOCK.time
+        self._creation_time = GLOBAL_CLOCK.time
 
     def collide(self, character: Character) -> None:
         if point_in_circle(character.position, character.size / 2, self.sprite.position) and self.live:
@@ -36,8 +36,32 @@ class Bullet:
         dy = self.velocity[1] * delta_time
         self.sprite.position = (self.sprite.position[0] + dx, self.sprite.position[1] + dy)
 
-        if self.creation_time + self.live_time < GLOBAL_CLOCK.time:
+        if self._creation_time + self.live_time < GLOBAL_CLOCK.time:
             self.live = False
 
     def draw(self) -> None:
         ...
+
+class BulletList:
+    def __init__(self, bullets: list[Bullet] | None = None) -> None:
+        self.bullets = bullets if bullets else []
+        self.sprite_list = SpriteList()
+
+    def spawn_bullet(self, bullet_type: type[Bullet], velocity: Point2 = (0, 0)) -> None:
+        new_bullet = bullet_type()
+        new_bullet.velocity = velocity
+        self.bullets.append(new_bullet)
+        self.sprite_list.append(new_bullet.sprite)
+
+    def update(self, delta_time: float) -> None:
+        for bullet in self.bullets:
+            bullet.update(delta_time)
+
+        for bullet in [b for b in self.bullets if not b.live]:
+            self.bullets.remove(bullet)
+            self.sprite_list.remove(bullet.sprite)
+
+    def draw(self) -> None:
+        self.sprite_list.draw()
+        for bullet in self.bullets:
+            bullet.draw()
